0565ddbb47081d73d2edbd72d794bf35d56308f0
Makoto <makoto@emurasoft.com>
2021-02-11 22:13:51 -0800

    Test

--- src/lib.rs
+++ src/lib.rs
@@ -182,6 +182,7 @@ impl API {
     pub fn queue_info(&mut self) -> Result<Vec<SlotQueueInfo>> {
         self.conn.exec("queue-info", &mut self.buf)?;
         let s = std::str::from_utf8(&self.buf)?;
+        dbg!(s);
         Ok(serde_json::from_str(pyon_to_json(s)?.as_str())?)
     }
 
